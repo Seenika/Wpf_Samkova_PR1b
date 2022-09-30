c15deb2bbc2a1a0727bbe554b9117f0ee5e1a45e
u31504 <u31504@P31504>
2022-09-30 14:35:11 +1000

    Кнопка на задание 4.5

--- Window6.xaml.cs
+++ Window6.xaml.cs
@@ -23,5 +23,12 @@ namespace Wpf_Samkova_PR1b
         {
             InitializeComponent();
         }
+
+        private void Win1_6_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            this.Close();
+        }
     }
 }
